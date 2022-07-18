72105851a60056de61ea297617d2e053218beea8
mohiuddin06617 <mohiuddin06617@gmail.com>
2022-07-19 01:14:54 +0600

    contain duplicates with using builtin set

--- contain_duplicates.py
+++ contain_duplicates.py
@@ -12,6 +12,8 @@ def contains_duplicate(nums: List[int]):
         pass
     return status
 
+def contains_duplicate_one_line_set(nums: List[int]):
+    return len(set(nums)) != len(nums)
 
 if __name__ == "__main__":
     print(contains_duplicate([2, 14, 18, 22, 22]))
