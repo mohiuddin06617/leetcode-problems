02cc7d2e0404d7d05849b4054788e68f3137960c
mahiuddin.ahmed <mahiuddin.ahmed@upaybd.com>
2022-07-21 14:05:21 +0600

    Best time to buy and sell stock with sliding window

--- best_time_buy_sell_stock.py
+++ best_time_buy_sell_stock.py
@@ -42,15 +42,28 @@ def max_profit_test(prices: List[int]):
             if diff > result:
                 result = diff
 
-
     return result
 
 
+def max_profit_sliding_window(prices: List[int]):
+    buy_price = prices[0]
+    max_profit = 0
+
+    for day, price in enumerate(prices):
+        if price < buy_price:
+            buy_price = price
+
+        if (price - buy_price) > max_profit:
+            max_profit = price - buy_price
+
+    return max_profit
+
+
 if __name__ == '__main__':
-    # print(max_profit([7, 6, 4, 3, 1]))
-    # print(max_profit([7, 1, 5, 3, 6, 4]))
-    # print(max_profit([2, 4, 1]))
-    # print(max_profit_test([3, 2, 6, 5, 0, 3]))  # expected 4
-    # print(max_profit_test([2, 1, 2, 1, 0, 1, 2]))  # expected 2
-    print(max_profit_test([3, 3, 5, 0, 0, 3, 1, 4]))  # expected 4
+    # print(max_profit_sliding_window([7, 6, 4, 3, 1]))  # expected 0
+    # print(max_profit_sliding_window([7, 1, 5, 3, 6, 4]))  # expected 5
+    # print(max_profit_sliding_window([2, 4, 1]))  # expected 2
+    # print(max_profit_sliding_window([3, 2, 6, 5, 0, 3]))  # expected 4
+    # print(max_profit_sliding_window([2, 1, 2, 1, 0, 1, 2]))  # expected 2
+    print(max_profit_sliding_window([3, 3, 5, 0, 0, 3, 1, 4]))  # expected 4
     # print(max_profit_test([3, 2, 6, 5, 0, 3]))  # expected 4
