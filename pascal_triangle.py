250934489aad44b6ad7c565ca14abc5d8ef51d9e
mohiuddin06617 <mohiuddin06617@gmail.com>
2022-07-23 12:43:06 +0600

    Pascal Triangle solved

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -3,20 +3,19 @@ from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        rows = [[1], [1, 1]]
-        row_num = 1
+        rows = [[1]]
+        for i in range(1, numRows):
+            rows.append([])
+            rows[i].append(1)
 
-        while row_num<numRows:
-            # row =
-            print(rows[row_num])
-        # for i in range(2, numRows):
-        #
-        #     rows.append([1] * (i + 1))
-            row_num += 1
+            for j in range(1, i):
+                rows[i].append(rows[i - 1][j - 1] + rows[i - 1][j])
+            if numRows != 0:
+                rows[i].append(1)
 
         return rows
 
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.generate(5))
+    print(solution.generate(20))
