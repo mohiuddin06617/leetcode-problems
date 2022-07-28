0192235343ca709ae4a85ff67f39bb8eeaafa2e1
mahiuddin.ahmed <mahiuddin.ahmed@upaybd.com>
2022-07-28 12:11:12 +0600

    leetCode 74 number problem solved

---
+++ search_a_2d_matrix.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        found = False
+        probable_row = None
+
+        for index, matrix_row in enumerate(matrix):
+            first_item = matrix_row[0]
+            last_item = matrix_row[-1]
+            if first_item <= target <= last_item:
+                probable_row = index
+                found = True
+
+        if found is False and probable_row is None:
+            return found
+
+        for index, value in enumerate(matrix[probable_row]):
+            if value == target:
+                return True
+
+        return False
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.searchMatrix(matrix=[[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target=3))
+    print(solution.searchMatrix(matrix=[[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target=13))
